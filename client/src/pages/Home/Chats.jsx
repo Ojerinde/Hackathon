0edd5a3231af526330dc2fbd8d3f3e5c4858d2e3
Joel Ojerinde <joelojerinde@gmail.com>
2023-02-07 13:03:41 +0100

    feat: add chat page

--- client/src/pages/Home/Chats.jsx
+++ client/src/pages/Home/Chats.jsx
@@ -1,2 +1,31 @@
-const Chats = () => {};
+import ChatItem from "./ChatItem";
+import classes from "./Chats.module.css";
+const Chats = () => {
+  const DummyData = [
+    {
+      name: "Joel Ojerinde",
+      message: "Hello",
+      time: "2023-01-03T21:31:17.178Z",
+    },
+    {
+      name: "Bolaji Ojerinde",
+      message: "What is up?",
+      time: "2023-06-03T21:31:17.178Z",
+    },
+  ];
+
+  return (
+    <ul className={classes.ul}>
+      {DummyData.map((item, index) => (
+        <ChatItem
+          key={index}
+          name={item.name}
+          userId={index}
+          message={item.message}
+          time={item.time}
+        />
+      ))}
+    </ul>
+  );
+};
 export default Chats;
