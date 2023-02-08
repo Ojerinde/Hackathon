9712d03fb44f120071d214fe0a571083e2921a59
Joel Ojerinde <joelojerinde@gmail.com>
2023-02-08 17:37:11 +0100

    feat: add more input fields

--- server/app.js
+++ server/app.js
@@ -1,41 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express') 
-const cookieParser = require('cookie-parser'); 
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
+const userRouter = require("./routes/userRoute");
 
+const app = express();
 
-const userRouter = require('./routes/userRoute')
-
-const app = express()
-
+// CORS for all origins
+app.use(cors());
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //routes
-app.get('/', (req,res) => {
-  res.status(200).json({message:"welcome to chatapp"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "welcome to chatapp" });
+});
 
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 
 // Handle error for unknown route
-app.use('*', (req, res) => {
-  return res.status(404).json({ message: 'route not found' })
-})
+app.use("*", (req, res) => {
+  return res.status(404).json({ message: "route not found" });
+});
 //Error-handling middleware
 app.use((err, req, res, next) => {
-    console.log(err)
-    // if (err.code === 11000) {
-    //   const errorStatus = 400,
-    //     err= "Email already exists"
-    // }
-  
-    const errorStatus = err.status || 500
-    res.status(errorStatus).send(err.message)
-    next()
-})
+  console.log(err);
+  // if (err.code === 11000) {
+  //   const errorStatus = 400,
+  //     err= "Email already exists"
+  // }
 
+  const errorStatus = err.status || 500;
+  res.status(errorStatus).send(err.message);
+  next();
+});
 
-module.exports = app
+module.exports = app;
